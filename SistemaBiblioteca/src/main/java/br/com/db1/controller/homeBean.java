92285c32d50ed8c97fe366b0f0f0248f9f57b469
BrendaRaylla <brenda_izui@hotmail.com>
2017-10-10 17:24:12 -0300

    tela home

---
+++ SistemaBiblioteca/src/main/java/br/com/db1/controller/homeBean.java
@@ -0,0 +1,19 @@
+package br.com.db1.controller;
+ 
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+ 
+@ManagedBean
+public class homeBean {
+     
+    public void buttonAction(ActionEvent actionEvent) {
+        addMessage("Bem vindo a biblioteca DB1!!");
+    }
+     
+    public void addMessage(String summary) {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary,  null);
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+}
