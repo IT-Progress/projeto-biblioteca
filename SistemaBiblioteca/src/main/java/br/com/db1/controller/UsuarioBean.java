3b29448d3b4417539bf8ccf0f26e2566883c31eb
camileharumi <camileharumi@gmail.com>
2017-10-14 00:28:00 -0300

    Criando tela de listagem de usuários

---
+++ SistemaBiblioteca/src/main/java/br/com/db1/controller/UsuarioBean.java
@@ -0,0 +1,94 @@
+package br.com.db1.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.application.FacesMessage.Severity;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import br.com.db1.dao.impl.UsuarioDao;
+import br.com.db1.model.Usuario;
+
+@RequestScoped
+@Named
+public class UsuarioBean {
+	@Inject
+	private UsuarioDao dao;
+
+	private List<Usuario> list;
+
+	private String nomeUsuarioFiltrado;
+
+	private Usuario usuario;
+	
+	@PostConstruct
+	public void init() {
+		list = new ArrayList<Usuario>();
+	}
+		
+	public String getNomeUsuarioFiltrado() {
+		return nomeUsuarioFiltrado;
+	}
+
+	public void setNomeUsuarioFiltrado(String nomeUsuarioFiltrado) {
+		this.nomeUsuarioFiltrado = nomeUsuarioFiltrado;
+	}
+
+	public List<Usuario> getList() {
+		return list;
+	}
+
+	public void setList(List<Usuario> list) {
+		this.list = list;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	
+	public void salvar() {
+		if (!dao.save(usuario)) {
+			adicionarMensagem("Erro ao cadastrar o Usuário.", FacesMessage.SEVERITY_ERROR);
+		}
+
+		adicionarMensagem("Usuário salvo com sucesso.", FacesMessage.SEVERITY_INFO);
+	}
+
+	public String editar() {
+		usuario = dao.findById(1L);
+		return "cadastrarUsuario";
+	}
+
+	public void remover(Usuario usuario) {
+		if (!dao.delete(usuario.getId())) {
+			adicionarMensagem("Erro ao remover o Usuário.", FacesMessage.SEVERITY_ERROR);
+		}
+
+		adicionarMensagem("Usuário removido com sucesso.", FacesMessage.SEVERITY_INFO);
+	}
+
+	public void listarUsuario() {
+		if (!nomeUsuarioFiltrado.isEmpty()) {
+			list.addAll(dao.findByName(nomeUsuarioFiltrado));
+		} else {
+			list.addAll(dao.findAll());
+		}
+	}
+
+	public void adicionarMensagem(String mensagem, Severity tipoMensagem) {
+		FacesContext fc = FacesContext.getCurrentInstance();
+		FacesMessage fm = new FacesMessage(mensagem);
+		fm.setSeverity(tipoMensagem);
+		fc.addMessage(null, fm);
+
+	}
+}
---
+++ SistemaBiblioteca/src/main/java/br/com/db1/model/Usuario.java
@@ -0,0 +1,60 @@
+package br.com.db1.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="usuario", schema="public")
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String nome;
+	
+	@Column
+	private String email;
+	
+	@Column
+	private String senha;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	
+}
