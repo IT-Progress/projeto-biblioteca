a7e52bb42a54e04ccdb887d9e61b2a7238f5455c
BrendaRaylla <brenda_izui@hotmail.com>
2017-10-16 16:59:16 -0200

    home com template

--- SistemaBiblioteca/src/main/java/br/com/db1/controller/homeBean.java
+++ SistemaBiblioteca/src/main/java/br/com/db1/controller/homeBean.java
@@ -1,19 +1,19 @@
 package br.com.db1.controller;
  
-import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
-import javax.faces.event.ActionEvent;
+import javax.servlet.http.HttpSession;
  
 @ManagedBean
 public class homeBean {
      
-    public void buttonAction(ActionEvent actionEvent) {
-        addMessage("Bem vindo a biblioteca DB1!!");
-    }
-     
-    public void addMessage(String summary) {
-        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary,  null);
-        FacesContext.getCurrentInstance().addMessage(null, message);
-    }
+	public String livrosAdm() {
+		FacesContext fc = FacesContext.getCurrentInstance();
+		ExternalContext ec = fc.getExternalContext();
+		HttpSession session = (HttpSession) ec.getSession(false);
+		session.removeAttribute("usuario");
+
+		return "/telaAdm";
+	}
 }
